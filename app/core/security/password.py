7708057990366aa95b6da6f33d3119aa7d92c482
charlestech-max <charles.tech0x77@outlook.com>
2026-03-25 21:33:28 -0400

    utf-8 config

--- app/core/security/password.py
+++ app/core/security/password.py
@@ -2,7 +2,6 @@ import bcrypt
 
 from app.core.config import get_settings
 
-
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
