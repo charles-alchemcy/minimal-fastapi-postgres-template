95678cd63592d626674ad6723634818e791626ca
charlestech-max <charles.tech0x77@outlook.com>
2026-03-25 21:33:13 -0400

    config update

--- app/core/security/password.py
+++ app/core/security/password.py
@@ -8,7 +8,6 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
 
-
 def get_password_hash(password: str) -> str:
     return bcrypt.hashpw(
         password.encode(),
