6601a4a2d36de191b1a185af77cb57f2ea2255c3
charlestech-max <charles.tech0x77@outlook.com>
2026-03-27 08:28:57 -0400

    change route format

--- app/api/deps.py
+++ app/api/deps.py
@@ -5,7 +5,6 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.api import api_messages
 from app.core import database_session
 from app.core.security.jwt import verify_jwt_token
